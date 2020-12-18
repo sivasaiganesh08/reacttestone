7a588594f9596c41b5bced93647c6d6359c344f9
sivasaiganesh medidi <sivasaiganesh08>
2020-12-18 12:50:30 +0530

    initial commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Welcome to SYSWORKS. Where you can get solutions for everything in IT.
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
